3988c61c5b8deddc7b6bdc12f9a5c810f6f2c001
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-09-10 02:17:51 +0530

    update version

--- app/src/main/java/com/example/xylophone/MainActivity.java
+++ app/src/main/java/com/example/xylophone/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,9 +31,38 @@ public class MainActivity extends AppCompatActivity {
 
         mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC,0);
 
-
+        csound = mSoundPool.load(getApplicationContext(),R.raw.c,1);
+        dsound = mSoundPool.load(getApplicationContext(),R.raw.d,1);
+        esound = mSoundPool.load(getApplicationContext(),R.raw.e,1);
+        fsound = mSoundPool.load(getApplicationContext(),R.raw.f,1);
+        gsound = mSoundPool.load(getApplicationContext(),R.raw.g,1);
+        asound = mSoundPool.load(getApplicationContext(),R.raw.a,1);
+        bsound = mSoundPool.load(getApplicationContext(),R.raw.b,1);
 
 
     }
+    public void playc(View v){
+        mSoundPool.play(csound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+    public void playd(View v){
+        mSoundPool.play(dsound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+    public void playe(View v){
+        mSoundPool.play(esound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+    public void playf(View v){
+        mSoundPool.play(fsound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+    public void playg(View v){
+        mSoundPool.play(gsound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+    public void playa(View v){
+        mSoundPool.play(asound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+    public void playb(View v){
+        mSoundPool.play(bsound,LEFT_VOLUME,RIGHT_VOLUME,priority,no_loop,normal_play_rate);
+    }
+
+
 
 }
